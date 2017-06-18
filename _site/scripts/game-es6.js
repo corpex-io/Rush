38d8dac7be7b1f8f0a653aca318e5cfc679a58d1
Donald Moore <donald@maximumdev.biz>
2017-06-18 15:34:56 -0500

    devcom

--- _site/scripts/game-es6.js
+++ _site/scripts/game-es6.js
@@ -116,7 +116,7 @@ class World extends createjs.Container {
 
     var hitEnemy = this.targetHitTestObjects(this.hero, this.enemies);
     if (hitEnemy !== false) {
-      console.log('hit enemy!', hitEnemy);
+      console.log('hit!', hitEnemy);
     }
 
     var hitCoin = this.targetHitTestObjects(this.hero, this.coins);
